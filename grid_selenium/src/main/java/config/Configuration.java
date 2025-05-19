bce7798d1b3144ed244372ab0de2665abcc2720d
tthuyen-batch47 <intern.tthuyen1@tma.com.vn>
2025-05-19 10:08:24 +0700

    refactor: Improve configuration

--- grid_selenium/src/main/java/config/Configuration.java
+++ grid_selenium/src/main/java/config/Configuration.java
@@ -3,13 +3,13 @@ package config;
 import java.util.Properties;
 
 public class Configuration {
-    private static Properties properties;
+    private static final Properties properties;
     static {
         properties = new Properties();
         try {
             properties.load(Configuration.class.getResourceAsStream("/config.properties"));
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException("Could not load config.properties", e);
         }
     }
 
